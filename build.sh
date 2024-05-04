75787ff0a5ff6794deda1f77975760ffb770f797
bozidarsk <bozidarkabahcijski@gmail.com>
2024-05-04 12:20:21 +0300

    Fixed scripts.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-sudo arduino-cli compile --fqbn arduino:avr:micro
+dotnet build
 if [[ $? -ne 0 ]]; then exit 1; fi
-sudo arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:micro
+
+arduino-cli compile --fqbn arduino:avr:micro
+if [[ $? -ne 0 ]]; then exit 1; fi
+
+arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:micro
