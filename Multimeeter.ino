06022271e4ff0b8ac27213163b5f8871b3968203
bozidarsk <bozidarkabahcijski@gmail.com>
2024-05-04 10:23:40 +0300

    Added ranges to measurements.

--- Multimeeter.ino
+++ Multimeeter.ino
@@ -1,25 +1,75 @@
-#define MAX_VALUE 10.0
 #define DELAY 100
+#define RANGE0 7
+#define RANGE1 8
+#define RANGE2 9
+#define RANGE3 10
 
-void reset() 
-{
-	Serial.println("reset");
-}
+#define DEBUG
+// #define HARDWARE_RANGE
+
+double maxes[] = { 5, 5e+2, 5e+1, 5e-1, 5e-2 };
+unsigned int range = 0;
 
 void setup() 
 {
 	Serial.begin(9600);
 
-	pinMode(3, INPUT_PULLUP);
-	attachInterrupt(digitalPinToInterrupt(3), reset, RISING);
+	#ifdef HARDWARE_RANGE
+	pinMode(RANGE0, INPUT);
+	pinMode(RANGE1, INPUT);
+	pinMode(RANGE2, INPUT);
+	pinMode(RANGE3, INPUT);
+	#endif
 }
 
 void loop() 
 {
-	double value = (double)analogRead(A5);
-	value /= 1023;
-	value *= MAX_VALUE;
-
-	Serial.println(value);
 	delay(DELAY);
+
+	#if HARDWARE_RANGE
+	range = 0;
+	range |= digitalRead(RANGE0) << 0;
+	range |= digitalRead(RANGE1) << 1;
+	range |= digitalRead(RANGE2) << 2;
+	range |= digitalRead(RANGE3) << 3;
+	range = (~range & 0xf) << 1;
+	range = (range != 0) ? (unsigned int)(log(range) / log(2)) : 0;
+	#else
+	if (Serial.available() > 0) 
+	{
+		range = 0;
+
+		while (Serial.available() > 2) 
+		{
+			range *= 10;
+			range += Serial.read() - 0x30;
+		}
+
+		Serial.read(); // 0x0d
+		Serial.read(); // 0x0a
+	}
+	#endif
+
+	double max = maxes[range];
+	double percentage = analogRead(A0) / 1023.0;
+	double value = percentage * max;
+
+	#ifdef DEBUG
+	Serial.print("range: ");
+	Serial.print(range);
+	Serial.print("  ");
+	Serial.print("max: ");
+	Serial.print(max, 6);
+	Serial.print("  ");
+	Serial.print("percentage: ");
+	Serial.print(percentage, 6);
+	Serial.print("  ");
+	Serial.print("value: ");
+	Serial.print(value, 6);
+	Serial.print("  ");
+
+	Serial.println("");
+	#else
+	Serial.println(value, 8);
+	#endif
 }
