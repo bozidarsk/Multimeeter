86ba658f38efb0f2be33fd69e0fa4a8c8f8ac2a9
bozidarsk <bozidarkabahcijski@gmail.com>
2024-05-17 14:56:59 +0300

    Not using aref pin.

--- Multimeeter.ino
+++ Multimeeter.ino
@@ -1,11 +1,26 @@
+const double internalRef = 2.5;
+
 void setup() 
 {
+	pinMode(A4, INPUT);
+	pinMode(A5, INPUT);
+
 	Serial.begin(9600, SERIAL_8N1);
-	analogReference(EXTERNAL);
 }
 
 void loop() 
 {
-	delay(50);
-	Serial.println(analogRead(A0) / 1023.0, 10);
+	delay(100);
+
+	double value = analogRead(A0) / 1023.0;
+
+	int range = (!digitalRead(A4) << 1) | !digitalRead(A5);
+	analogReference((range == 0) ? DEFAULT : INTERNAL);
+
+	if (range == 0b01)
+		value *= internalRef / 0.5;
+	if (range == 0b11)
+		value *= internalRef / 0.05;
+
+	Serial.println(value, 10);
 }
